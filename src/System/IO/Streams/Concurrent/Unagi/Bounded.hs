4f321227ff55f722a2e68f85e4d854e155cf20c2
Luke Hoersten <luke@hoersten.org>
2014-12-18 10:56:53 -0600

    Removed opaque type for chan dupping.

--- src/System/IO/Streams/Concurrent/Unagi/Bounded.hs
+++ src/System/IO/Streams/Concurrent/Unagi/Bounded.hs
@@ -6,25 +6,20 @@ module System.IO.Streams.Concurrent.Unagi.Bounded
        , chanToInput
        , chanToOutput
        , makeChanPipe
-       , dupStream
-       , DupHandle
        ) where
 
 
 ------------------------------------------------------------------------------
-import           Control.Applicative                   (pure, (<$>), (<*>))
+import           Control.Applicative                   ((<$>), (<*>))
 import           Control.Concurrent.Chan.Unagi.Bounded (InChan, OutChan,
-                                                        dupChan, newChan,
-                                                        readChan, writeChan)
-import           Control.Monad                         ((>=>))
+                                                        newChan, readChan,
+                                                        writeChan)
 import           Prelude                               hiding (read)
 import           System.IO.Streams.Internal            (InputStream,
                                                         OutputStream,
                                                         makeInputStream,
                                                         makeOutputStream, read)
 
-
-newtype DupHandle a = DupHandle { unDupHandle :: InChan (Maybe a) }
 
 ------------------------------------------------------------------------------
 -- | Writes the contents of an input stream to a channel until the input stream
@@ -58,15 +53,7 @@ chanToOutput = makeOutputStream . writeChan
 --
 -- Since reading from the 'InputStream' and writing to the 'OutputStream' are
 -- blocking calls, be sure to do so in different threads.
-makeChanPipe :: Int -> IO (InputStream a, OutputStream a, DupHandle a)
+makeChanPipe :: Int -> IO (InputStream a, OutputStream a)
 makeChanPipe size = do
     (inChan, outChan) <- newChan size
-    (,,) <$> chanToInput outChan <*> chanToOutput inChan <*> pure (DupHandle inChan)
-
-
-------------------------------------------------------------------------------
--- | Use a 'DupHandle' to replicate everything written on the
--- associated 'OutputStream' to the 'InputStream'.
---
-dupStream :: DupHandle a -> IO (InputStream a)
-dupStream = dupChan . unDupHandle >=> chanToInput
+    (,) <$> chanToInput outChan <*> chanToOutput inChan
